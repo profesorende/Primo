998e798bec4c828414b352cb0e8d477e70689cc8
Pilar de las Heras <Pilar de las Heras@PilardelasHeras>
2015-04-14 08:52:02 +0200

    version 1. Primera versión del proyecto Primo

---
+++ src/Esprimo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Esprimo {
+	
+	public static void main (String [] ar){
+		int numero;
+		boolean resultado;
+		Scanner teclado= new Scanner (System.in);
+		System.out.print("por favor introduzca un número entero positivo  :: ");
+		numero=teclado.nextInt();
+		resultado = Esprimo.esprimo(numero);
+		if (resultado == true){
+			System.out.print("El número introducido es PRIMO");
+		}
+		else{
+			System.out.print("El número introducido NO es primo");
+		}
+	}
+	
+	public static boolean esprimo(int num){
+		if (num>0){
+			boolean primo=true;
+			for (int i=2; i<num; i++){
+				if (num%i==0){primo=false;}	
+			}
+			return primo;
+		}
+		else{
+			Esprimo.error("número negativo");
+			return false;}
+	}
+	
+	public static void error(String s){
+		System.out.print(s);
+	}
+}
